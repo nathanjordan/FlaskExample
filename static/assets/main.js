aa0192df727d91fabceb85cee217ce7ea07168f4
Nathan Jordan <natedagreat27274@gmail.com>
2013-10-03 18:47:00 -0700

    added some new libraries

--- static/assets/main.js
+++ static/assets/main.js
@@ -60,17 +60,20 @@ function refreshUsers() {
         var name, html, button;
         // This removes the previous list of users
         $('#userList').html('');
-        // Loop over the new users we got from the database
-        for(var i = 0; i < users.length; i++) {
+        // This is an underscore function. Instead of using a for loop,
+        // we can use the each function to iterate over the list. It makes
+        // The code look cleaner and can help prevent errors related to array indices, etc.
+        _.each(users, function(user) {
             // Create the text that will be displayed
-            name = users[i].username + ':' + users[i].firstName + ', ' + users[i].lastName;
+            name = user.username + ':' + user.firstName + ', ' + user.lastName;
             // create the delete button with a data-ref attribute set to the username
-            button = '<button data-ref="' + users[i].username + '">Delete</button>';
+            button = '<button data-ref="' + user.username + '">Delete</button>';
             // finally wrap it in a 'list item' tag (its going in an ordered list)
             html = '<li>' + name + button + '</li>';
             // Put it inside the list element
             $('#userList').append(html);
-        }
+
+        });
         // Now lets register these new delete buttons so they work (the old
         // ones are now invalid, since they don't exist!)
         registerDeleteButtons();
